34625eb47d256069d1c703e3ed62528dcade17a5
data2coordi <data2coordi@gmail.com>
2023-12-13 23:38:15 +0900

    merge

--- check_my_color.php
+++ check_my_color.php
@@ -9,8 +9,8 @@ namespace check_my_color;
  Author URI: https://color.toshidayurika.com
  License: GPLv2 or later
  Text Domain: contraction
- 
-*/
+
+ */
 
 
 require_once("ColorCheckerClass.php");
@@ -18,41 +18,41 @@ require_once("ColorCheckerClass.php");
 
 Class  Simple_color_class{
 
-	public function __construct() {
-		add_filter( 'the_content', array($this, 'simple_check_my_color'));
-		add_action( 'pre_get_posts', array($this, 'simple_check_displayRet'));
+public function __construct() {
+	add_filter( 'the_content', array($this, 'simple_check_my_color'));
+	add_action( 'pre_get_posts', array($this, 'simple_check_displayRet'));
+}
+
+public function simple_check_displayRet( $query ) {
+
+	if ( is_admin() ) return;
+
+	if ( is_page('dia-result')) {
+		$ColorChecker = new ColorCheckerClass();
+		$check_sheet_csv = wp_upload_dir()['basedir']  . "/p_color_check_sheet.csv";
+
+		$ColorChecker->SetCheckFile($check_sheet_csv);
+		$ret = $ColorChecker->DisplayRet( $_POST['result'], $_GET['id']);
+
+		$query->set( 'cat', get_cat_ID( $ret ));
+		return;
 	}
+}
 
-	public function simple_check_displayRet( $query ) {
-		
-		if ( is_admin() ) return;
 
-		if ( is_page('dia-result')) {
-			$ColorChecker = new ColorCheckerClass();
-			$check_sheet_csv = wp_upload_dir()['basedir']  . "/p_color_check_sheet.csv";
-			
-			$ColorChecker->SetCheckFile($check_sheet_csv);
-			$ret = $ColorChecker->DisplayRet( $_POST['result'], $_GET['id']);
+public function simple_check_my_color($content){
 
-			$query->set( 'cat', get_cat_ID( $ret ));
-			return;
+
+	if (is_page('diagnosis')){
+		if (!isset($_GET['id']))  {
+			return $content;  
 		}
+
+		$ColorChecker = new ColorCheckerClass();
+		$content = $ColorChecker->Replace($content, $_POST['result'], $_POST['page_no'], $_GET['id']);
 	}
-
-
-	public function simple_check_my_color($content){
-
-
-		if (is_page('diagnosis')){
-			if (!isset($_GET['id']))  {
-				return $content;  
-			}
-
-			$ColorChecker = new ColorCheckerClass();
-			$content = $ColorChecker->Replace($content, $_POST['result'], $_POST['page_no'], $_GET['id']);
-		}
-		return $content;  
-	}
+	return $content;  
+}
 }
 
 
